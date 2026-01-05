868fe77462b389959c6dd4fb23fb7c24e253f800
Fadel M <fadel979797@gmail.com>
2026-01-05 11:37:35 +0700

    logrus

---
+++ logger.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+var log = logrus.New()
+
+func init() {
+	log.SetOutput(os.Stdout)
+	log.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp:   true,
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+	log.SetLevel(logrus.InfoLevel)
+}
